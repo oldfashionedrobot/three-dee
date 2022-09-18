a732730f6651f052515dbe291b57b737eb8b7ec0
Sanjeev Nayak <snayak00+ofrgit@gmail.com>
2022-09-18 15:39:39 -0400

    i dunno

--- webpack.development.js
+++ webpack.development.js
@@ -1,5 +1,5 @@
 const { merge } = require("webpack-merge");
-const common = require("../webpack.config");
+const common = require("./webpack.config,js");
 
 const BundleAnalyzerPlugin =
   require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
--- webpack.production.js
+++ webpack.production.js
@@ -1,10 +1,9 @@
 const { merge } = require("webpack-merge");
-const common = require("../webpack.config.js");
+const common = require("./webpack.config.js");
 
 module.exports = merge(common, {
   mode: "production",
   optimization: {
     // minimize: false,
-    minimizer: []
   }
 });
