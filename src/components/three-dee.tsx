f4e7dacde91eecc1bb8e15b6c023236af5f94917
Sanjeev Nayak <snayak00+ofrgit@gmail.com>
2022-11-29 09:24:36 -0500

    super basic babylon js

--- src/components/three-dee.tsx
+++ src/components/three-dee.tsx
@@ -1,12 +1,56 @@
-import React, { useEffect, useRef, useState } from "react";
-import { init3d } from "../utils/3d-service";
+import React, { useEffect, useRef } from 'react';
+import * as BABYLON from '@babylonjs/core/Legacy/legacy';
 
 export default function ThreeDee() {
   const canvasRef: React.MutableRefObject<HTMLCanvasElement> = useRef(null);
+  const fpsRef: React.MutableRefObject<HTMLDivElement> = useRef(null);
 
   useEffect(() => {
-    return init3d(canvasRef);
+    const engine = new BABYLON.Engine(canvasRef.current, true); // Generate the BABYLON 3D engine
+
+    // Add your code here matching the playground format
+    const createScene = () => {
+      const scene = new BABYLON.Scene(engine);
+
+      const camera = new BABYLON.ArcRotateCamera(
+        'camera',
+        -Math.PI / 2,
+        Math.PI / 2.5,
+        3,
+        new BABYLON.Vector3(0, 0, 0),
+        scene
+      );
+      camera.attachControl(canvasRef.current, true);
+
+      const light = new BABYLON.HemisphericLight(
+        'light',
+        new BABYLON.Vector3(0, 1, 0),
+        scene
+      );
+
+      const box = BABYLON.MeshBuilder.CreateBox('box', {}, scene);
+
+      return scene;
+    };
+
+    const scene = createScene(); //Call the createScene function
+
+    // Register a render loop to repeatedly render the scene
+    engine.runRenderLoop(function () {
+      fpsRef.current.innerHTML = engine.getFps().toFixed();
+      scene.render();
+    });
+
+    // Watch for browser/canvas resize events
+    window.addEventListener('resize', function () {
+      engine.resize();
+    });
   }, []);
 
-  return <canvas ref={canvasRef}></canvas>;
+  return (
+    <>
+      <div ref={fpsRef}></div>
+      <canvas ref={canvasRef}></canvas>
+    </>
+  );
 }
--- src/utils/3d-service.ts
+++ src/utils/3d-service.ts
@@ -1,182 +1,182 @@
-import * as THREE from 'three';
-import { FBXLoader } from 'three/addons/loaders/FBXLoader.js';
-// import { OrbitControls } from "three/addons/controls/OrbitControls.js";
-import { EffectComposer } from 'three/addons/postprocessing/EffectComposer.js';
-import { RenderPass } from 'three/addons/postprocessing/RenderPass.js';
-// import { GlitchPass } from "three/addons/postprocessing/GlitchPass.js";
-import { UnrealBloomPass } from 'three/addons/postprocessing/UnrealBloomPass.js';
+// import * as THREE from 'three';
+// import { FBXLoader } from 'three/addons/loaders/FBXLoader.js';
+// // import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+// import { EffectComposer } from 'three/addons/postprocessing/EffectComposer.js';
+// import { RenderPass } from 'three/addons/postprocessing/RenderPass.js';
+// // import { GlitchPass } from "three/addons/postprocessing/GlitchPass.js";
+// import { UnrealBloomPass } from 'three/addons/postprocessing/UnrealBloomPass.js';
 
-// import checkerImg from "../assets/images/checker.png";
-import crystalFbx from '../assets/models/crystals_one_mat.fbx';
-import crystalNormalMap from '../assets/images/crystals_Normal.png';
-import crystalAlphaMap from '../assets/images/crystals_AlbedoTransparency.png';
-import crystalRedTex from '../assets/images/crystals_Emission_red.png';
-import crystalGreenTex from '../assets/images/crystals_Emission_green.png';
-import crystalBlueTex from '../assets/images/crystals_Emission_blue.png';
-import crystalPurpleTex from '../assets/images/crystals_Emission_purple.png';
+// // import checkerImg from "../assets/images/checker.png";
+// import crystalFbx from '../assets/models/crystals_one_mat.fbx';
+// import crystalNormalMap from '../assets/images/crystals_Normal.png';
+// import crystalAlphaMap from '../assets/images/crystals_AlbedoTransparency.png';
+// import crystalRedTex from '../assets/images/crystals_Emission_red.png';
+// import crystalGreenTex from '../assets/images/crystals_Emission_green.png';
+// import crystalBlueTex from '../assets/images/crystals_Emission_blue.png';
+// import crystalPurpleTex from '../assets/images/crystals_Emission_purple.png';
 
-import Stats from '../lib/stats';
+// import Stats from '../lib/stats';
 
-export function init3d(
-  canvasRef: React.MutableRefObject<HTMLCanvasElement>,
-  showStats = true
-) {
-  let stats: any;
-  if (showStats) {
-    stats = Stats();
-    stats.showPanels(0, 1, 2); // 0: fps, 1: ms, 2: mb, 3+: custom
-    document.body.appendChild(stats.dom);
-  }
+// export function init3d(
+//   canvasRef: React.MutableRefObject<HTMLCanvasElement>,
+//   showStats = true
+// ) {
+//   let stats: any;
+//   if (showStats) {
+//     stats = Stats();
+//     stats.showPanels(0, 1, 2); // 0: fps, 1: ms, 2: mb, 3+: custom
+//     document.body.appendChild(stats.dom);
+//   }
 
-  // INIT RENDERER
-  const renderer = new THREE.WebGLRenderer({
-    canvas: canvasRef.current,
-    alpha: true,
-    premultipliedAlpha: false
-  });
+//   // INIT RENDERER
+//   const renderer = new THREE.WebGLRenderer({
+//     canvas: canvasRef.current,
+//     alpha: true,
+//     premultipliedAlpha: false
+//   });
 
-  // INIT CAMERA
-  const fov = 40,
-    aspect = 2 /* the canvas default */,
-    near = 0.1,
-    far = 1000;
-  const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-  camera.position.set(0, 0, 10);
+//   // INIT CAMERA
+//   const fov = 40,
+//     aspect = 2 /* the canvas default */,
+//     near = 0.1,
+//     far = 1000;
+//   const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+//   camera.position.set(0, 0, 10);
 
-  // INIT CONTROLS
-  // const controls = new OrbitControls(camera, canvasRef.current);
-  // controls.target.set(0, 5, 0);
-  // controls.update();
+//   // INIT CONTROLS
+//   // const controls = new OrbitControls(camera, canvasRef.current);
+//   // controls.target.set(0, 5, 0);
+//   // controls.update();
 
-  // INIT SCENE
-  const scene = new THREE.Scene();
-  scene.background = new THREE.Color(0x000000);
+//   // INIT SCENE
+//   const scene = new THREE.Scene();
+//   scene.background = new THREE.Color(0x000000);
 
-  // INIT POST PROCESSING
-  const composer = new EffectComposer(renderer),
-    renderPass = new RenderPass(scene, camera),
-    effectPass = new UnrealBloomPass(
-      new THREE.Vector2(window.innerWidth, window.innerHeight),
-      1, // strength
-      2, // radius
-      0.6 // threshold
-    );
-  composer.addPass(renderPass);
-  composer.addPass(effectPass);
+//   // INIT POST PROCESSING
+//   const composer = new EffectComposer(renderer),
+//     renderPass = new RenderPass(scene, camera),
+//     effectPass = new UnrealBloomPass(
+//       new THREE.Vector2(window.innerWidth, window.innerHeight),
+//       1, // strength
+//       2, // radius
+//       0.6 // threshold
+//     );
+//   composer.addPass(renderPass);
+//   composer.addPass(effectPass);
 
-  const color = 0xffffff,
-    ambientLight = new THREE.AmbientLight(color, 0.2),
-    light = new THREE.DirectionalLight(color, 1);
-  light.position.set(-1, 2, 4);
-  light.rotation.set(45, 0, 0);
-  scene.add(ambientLight);
-  scene.add(light);
+//   const color = 0xffffff,
+//     ambientLight = new THREE.AmbientLight(color, 0.2),
+//     light = new THREE.DirectionalLight(color, 1);
+//   light.position.set(-1, 2, 4);
+//   light.rotation.set(45, 0, 0);
+//   scene.add(ambientLight);
+//   scene.add(light);
 
-  // FBX testing
-  const normalTex = new THREE.TextureLoader().load(crystalNormalMap);
-  const alphaTex = new THREE.TextureLoader().load(crystalAlphaMap);
-  const colorMaps = [
-    crystalRedTex,
-    crystalBlueTex,
-    crystalGreenTex,
-    crystalPurpleTex
-  ];
+//   // FBX testing
+//   const normalTex = new THREE.TextureLoader().load(crystalNormalMap);
+//   const alphaTex = new THREE.TextureLoader().load(crystalAlphaMap);
+//   const colorMaps = [
+//     crystalRedTex,
+//     crystalBlueTex,
+//     crystalGreenTex,
+//     crystalPurpleTex
+//   ];
 
-  const objs: THREE.Mesh[] = [];
-  new FBXLoader().load(
-    crystalFbx,
-    (object: any) => {
-      // console.log(object.children)
-      // const [wide1, wide2, _, long1, long2] = object.children;
+//   const objs: THREE.Mesh[] = [];
+//   new FBXLoader().load(
+//     crystalFbx,
+//     (object: any) => {
+//       // console.log(object.children)
+//       // const [wide1, wide2, _, long1, long2] = object.children;
 
-      object.children.splice(2, 1);
-      const children = object.children.map(
-        (child: THREE.Mesh, index: number) => {
-          child.scale.set(0.1, 0.1, 0.1);
-          child.position.set(index * 2 - 3, -1, 0);
+//       object.children.splice(2, 1);
+//       const children = object.children.map(
+//         (child: THREE.Mesh, index: number) => {
+//           child.scale.set(0.1, 0.1, 0.1);
+//           child.position.set(index * 2 - 3, -1, 0);
 
-          const tex = new THREE.TextureLoader().load(colorMaps[index]);
-          child.material = new THREE.MeshPhongMaterial({
-            map: tex,
-            emissiveMap: tex,
-            emissive: index === 2 ? '#777' : '#fff',
-            emissiveIntensity: 1.7,
-            alphaMap: alphaTex,
-            normalMap: normalTex
-          });
+//           const tex = new THREE.TextureLoader().load(colorMaps[index]);
+//           child.material = new THREE.MeshPhongMaterial({
+//             map: tex,
+//             emissiveMap: tex,
+//             emissive: index === 2 ? '#777' : '#fff',
+//             emissiveIntensity: 1.7,
+//             alphaMap: alphaTex,
+//             normalMap: normalTex
+//           });
 
-          return child;
-        }
-      );
+//           return child;
+//         }
+//       );
 
-      objs.push(...children);
-      scene.add(...children);
-    },
-    (xhr: ProgressEvent) => {
-      console.log((xhr.loaded / xhr.total) * 100 + '% loaded');
-    },
-    (error: Error) => {
-      console.log(error);
-    }
-  );
+//       objs.push(...children);
+//       scene.add(...children);
+//     },
+//     (xhr: ProgressEvent) => {
+//       console.log((xhr.loaded / xhr.total) * 100 + '% loaded');
+//     },
+//     (error: Error) => {
+//       console.log(error);
+//     }
+//   );
 
-  let requestFrameId: number;
-  (function render(time?: number) {
-    stats.begin();
+//   let requestFrameId: number;
+//   (function render(time?: number) {
+//     stats.begin();
 
-    time *= 0.001; // convert time to seconds
+//     time *= 0.001; // convert time to seconds
 
-    if (resizeRendererToDisplaySize(renderer, composer)) {
-      const canvas = renderer.domElement;
-      camera.aspect = canvas.clientWidth / canvas.clientHeight;
-      camera.updateProjectionMatrix();
-    }
+//     if (resizeRendererToDisplaySize(renderer, composer)) {
+//       const canvas = renderer.domElement;
+//       camera.aspect = canvas.clientWidth / canvas.clientHeight;
+//       camera.updateProjectionMatrix();
+//     }
 
-    objs.forEach((obj, idx) => {
-      const speed = 1 + idx * 0.1;
-      const rot = time * speed;
-      // obj.rotation.x = rot;
-      obj.rotation.y = rot;
-    });
+//     objs.forEach((obj, idx) => {
+//       const speed = 1 + idx * 0.1;
+//       const rot = time * speed;
+//       // obj.rotation.x = rot;
+//       obj.rotation.y = rot;
+//     });
 
-    // renderer.render(scene, camera);
-    composer.render();
+//     // renderer.render(scene, camera);
+//     composer.render();
 
-    requestFrameId = requestAnimationFrame(render);
+//     requestFrameId = requestAnimationFrame(render);
 
-    stats.end();
-  })();
+//     stats.end();
+//   })();
 
-  return () => {
-    [renderer, normalTex, alphaTex, ambientLight, light, ...objs].forEach(
-      (x: any) => {
-        // TODO: use some type guards / inferences here
-        // TODO: not sure if this is correct/enough cleanup
-        // console.log(x.material);
-        x.material?.map.dispose();
-        x.material?.dispose();
-        x.dispose();
-      }
-    );
-    document.body.removeChild(stats.dom);
-    cancelAnimationFrame(requestFrameId);
-  };
-}
+//   return () => {
+//     [renderer, normalTex, alphaTex, ambientLight, light, ...objs].forEach(
+//       (x: any) => {
+//         // TODO: use some type guards / inferences here
+//         // TODO: not sure if this is correct/enough cleanup
+//         // console.log(x.material);
+//         x.material?.map.dispose();
+//         x.material?.dispose();
+//         x.dispose();
+//       }
+//     );
+//     document.body.removeChild(stats.dom);
+//     cancelAnimationFrame(requestFrameId);
+//   };
+// }
 
-function resizeRendererToDisplaySize(
-  renderer: THREE.Renderer,
-  composer: EffectComposer
-) {
-  const canvas = renderer.domElement;
-  const pixelRatio = window.devicePixelRatio;
-  const width = (canvas.clientWidth * pixelRatio) | 0;
-  const height = (canvas.clientHeight * pixelRatio) | 0;
-  // const width = canvas.clientWidth;
-  // const height = canvas.clientHeight;
-  const needResize = canvas.width !== width || canvas.height !== height;
-  if (needResize) {
-    renderer.setSize(width, height, false);
-    composer.setSize(width, height);
-  }
-  return needResize;
-}
+// function resizeRendererToDisplaySize(
+//   renderer: THREE.Renderer,
+//   composer: EffectComposer
+// ) {
+//   const canvas = renderer.domElement;
+//   const pixelRatio = window.devicePixelRatio;
+//   const width = (canvas.clientWidth * pixelRatio) | 0;
+//   const height = (canvas.clientHeight * pixelRatio) | 0;
+//   // const width = canvas.clientWidth;
+//   // const height = canvas.clientHeight;
+//   const needResize = canvas.width !== width || canvas.height !== height;
+//   if (needResize) {
+//     renderer.setSize(width, height, false);
+//     composer.setSize(width, height);
+//   }
+//   return needResize;
+// }
