add54ddd55287265c8729c5b228bc4fa5deaf2ae
Sanjeev Nayak <snayak00+ofrgit@gmail.com>
2022-12-06 20:21:22 -0500

    step 1 of tut

--- src/components/three-dee.tsx
+++ src/components/three-dee.tsx
@@ -1,50 +1,45 @@
 import React, { useEffect, useRef } from 'react';
-import * as BABYLON from '@babylonjs/core/Legacy/legacy';
+import '@babylonjs/core/Debug/debugLayer';
+import '@babylonjs/inspector';
+import '@babylonjs/loaders/glTF';
+import {
+  Engine,
+  Scene,
+  ArcRotateCamera,
+  Vector3,
+  HemisphericLight,
+  Mesh,
+  MeshBuilder
+} from '@babylonjs/core';
+// import * as BABYLON from '@babylonjs/core/Legacy/legacy';
 
 export default function ThreeDee() {
   const canvasRef: React.MutableRefObject<HTMLCanvasElement> = useRef(null);
   const fpsRef: React.MutableRefObject<HTMLDivElement> = useRef(null);
 
+  const engineRef: React.MutableRefObject<Engine> = useRef(null);
+  const sceneRef: React.MutableRefObject<Scene> = useRef(null);
+
   useEffect(() => {
-    const engine = new BABYLON.Engine(canvasRef.current, true); // Generate the BABYLON 3D engine
-
-    // Add your code here matching the playground format
-    const createScene = () => {
-      const scene = new BABYLON.Scene(engine);
-
-      const camera = new BABYLON.ArcRotateCamera(
-        'camera',
-        -Math.PI / 2,
-        Math.PI / 2.5,
-        3,
-        new BABYLON.Vector3(0, 0, 0),
-        scene
-      );
-      camera.attachControl(canvasRef.current, true);
-
-      const light = new BABYLON.HemisphericLight(
-        'light',
-        new BABYLON.Vector3(0, 1, 0),
-        scene
-      );
-
-      const box = BABYLON.MeshBuilder.CreateBox('box', {}, scene);
-
-      return scene;
-    };
-
-    const scene = createScene(); //Call the createScene function
+    engineRef.current = new Engine(canvasRef.current, true); // Generate the BABYLON 3D engine
+    sceneRef.current = createScene(engineRef.current); //Call the createScene function
 
     // Register a render loop to repeatedly render the scene
-    engine.runRenderLoop(function () {
-      fpsRef.current.innerHTML = engine.getFps().toFixed();
-      scene.render();
-    });
+    engineRef.current.runRenderLoop(handleRender);
 
-    // Watch for browser/canvas resize events
-    window.addEventListener('resize', function () {
-      engine.resize();
-    });
+    window.addEventListener('resize', handleResize);
+    window.addEventListener('keyup', handleKeyup);
+
+    return () => {
+      // engineRef.current.stopRenderLoop(handleRender);
+      engineRef.current.dispose();
+      engineRef.current = undefined;
+      sceneRef.current.dispose();
+      sceneRef.current = undefined;
+
+      window.removeEventListener('resize', handleResize);
+      window.removeEventListener('keyup', handleKeyup);
+    };
   }, []);
 
   return (
@@ -53,4 +48,49 @@ export default function ThreeDee() {
       <canvas ref={canvasRef}></canvas>
     </>
   );
+
+  function handleRender() {
+    fpsRef.current.innerHTML = engineRef.current.getFps().toFixed();
+    sceneRef.current.render();
+  }
+
+  function handleKeyup(ev) {
+    // hide/show the Inspector
+    // Shift+I
+    if (ev.shiftKey && ev.key === 'I') {
+      console.log(sceneRef.current.debugLayer.isVisible());
+      if (sceneRef.current.debugLayer.isVisible()) {
+        sceneRef.current.debugLayer.hide();
+      } else {
+        sceneRef.current.debugLayer.show();
+      }
+    }
+  }
+
+  function handleResize() {
+    // Watch for browser/canvas resize events
+    engineRef.current.resize();
+  }
+
+  function createScene(engine) {
+    const scene = new Scene(engine);
+
+    const camera: ArcRotateCamera = new ArcRotateCamera(
+      'Camera',
+      Math.PI / 2,
+      Math.PI / 2,
+      2,
+      Vector3.Zero(),
+      scene
+    );
+    camera.attachControl(canvasRef.current, true);
+    const light1: HemisphericLight = new HemisphericLight(
+      'light1',
+      new Vector3(1, 1, 0),
+      scene
+    );
+    const box: Mesh = MeshBuilder.CreateBox('box', {}, scene);
+
+    return scene;
+  }
 }
